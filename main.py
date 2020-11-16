7869f698300b17a67859de9a501009f31dab5a45
Hồ Thiên Long <long.ho460@hcmut.edu.vn>
2020-11-16 04:06:00 +0000

    initial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 from functools import reduce
+
 def khoang_cach_giua_2_diem(diem1, diem2):
   return math.sqrt((diem2[0]- diem1[0])**2 + (diem2[1] - diem1[1])**2)
 
@@ -20,25 +21,6 @@ class Order:
     print([self.toado[0], self.toado[1], self.thetich, self.trongluong])
 
 
-file1 = open("input.txt","r") 
-input_file = file1.read()
-lines = input_file.split("\n")
-khoA = list(map(int, lines[0].split(" ")))
-print(khoA)
-
-N = int(lines[1].split(" ")[0])
-M = int(lines[1].split(" ")[1])
-
-order_list = []
-for i in range (2,N+2):
-  id = i - 2
-  toado = [int(lines[i].split(" ")[0]), int(lines[i].split(" ")[1])]
-  thetich = int(lines[i].split(" ")[2])
-  trongluong = int(lines[i].split(" ")[3])
-  order = Order(id, toado, thetich, trongluong)
-  order_list.append(order)
-
-
 
 def ham_luong_gia(lstOrder, khoA): 
   quangduong = khoang_cach_giua_2_diem(lstOrder[0].toado, khoA)
@@ -56,7 +38,28 @@ def ham_luong_gia(lstOrder, khoA):
   return loinhuan
 
 
+def main():
+  file1 = open("input.txt","r") 
+  input_file = file1.read()
+  lines = input_file.split("\n")
+  khoA = list(map(int, lines[0].split(" ")))
+  print(khoA)
 
-print_order_list(order_list)
-print(order_list[0].toado)
-print(ham_luong_gia(order_list, khoA))
+  N = int(lines[1].split(" ")[0])
+  M = int(lines[1].split(" ")[1])
+
+  order_list = []
+  for i in range (2,N+2):
+    id = i - 2
+    toado = [int(lines[i].split(" ")[0]), int(lines[i].split(" ")[1])]
+    thetich = int(lines[i].split(" ")[2])
+    trongluong = int(lines[i].split(" ")[3])
+    order = Order(id, toado, thetich, trongluong)
+    order_list.append(order)
+ 
+  print_order_list(order_list)
+  print(order_list[0].toado)
+  print(ham_luong_gia(order_list, khoA))
+
+
+main()
