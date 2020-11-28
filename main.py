aaf3e936ecc2fe0d6d8da32801e1e613d0e474c4
Hồ Thiên Long <long.ho460@hcmut.edu.vn>
2020-11-28 03:57:24 +0000

    add hillclimbing algorithm

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import math
-from functools import reduce
+from functools import reduce # sử dụng khi param cùng kiểu với return
 import random
+import copy
 
 file1 = open("input.txt","r") 
 input_file = file1.read()
 lines = input_file.split("\n")
 khoA = list(map(int, lines[0].split(" ")))
-print(khoA)
+
 
 N = int(lines[1].split(" ")[0])
 M = int(lines[1].split(" ")[1])
@@ -48,7 +49,7 @@ def loi_nhuan(lstOrder):
   chiphi = quangduong / 40 * 20 + 10
   loinhuan = doanhthu - chiphi
   return loinhuan
-  
+
 
 def ham_luong_gia(lstOrder):
   lstNhanVien = {}
@@ -56,11 +57,9 @@ def ham_luong_gia(lstOrder):
     current_nhanvien = lstOrder[i].id_nhanvien
     try:
       lstNhanVien[current_nhanvien] += [lstOrder[i]]
-      print("add")
     except:
       lstNhanVien[current_nhanvien] = [lstOrder[i]]
-      print("new")
-
+      
   luonggia = 0
   for i in lstNhanVien:
     for j in lstNhanVien:
@@ -68,12 +67,21 @@ def ham_luong_gia(lstOrder):
       luonggia += abs(loi_nhuan(lstNhanVien[i]) - loi_nhuan(lstNhanVien[j]))
   return luonggia
 
-   
+
 def initialize_state(order_list, number_nhanvien):
-  for i in range (len(order_list)):
+  assigned = []
+  lstIdOrder = [i for i in range(len(order_list))]
+  for i in range(number_nhanvien):
+    lstChoice = list(set(lstIdOrder) - set(assigned))
+    ran = random.choice(lstChoice)
+    order_list[ran].gan_nhanvien(i)
+    assigned.append(ran)
+  lstNotAssigned = list(set(lstIdOrder) - set(assigned))
+  for i in lstNotAssigned:
     order_list[i].gan_nhanvien(random.randrange(number_nhanvien))
     
-def print_result(order_list, number_nhanvien):
+
+def write_result(order_list):
   f = open("output.txt",'w')
   # sort theo id_nhanvien => in xong nhan vien dau thi \n
   order_list.sort(key = lambda x: x.id_nhanvien)
@@ -89,6 +97,65 @@ def print_result(order_list, number_nhanvien):
       f.write('\n')
       current_nhanvien += 1
 
+
+def checkTotal(lstOrder, M):
+  lstCheck = []
+  for i in range(M):
+    lstCheck.append(False)
+  for x in lstOrder:
+    lstCheck[x.id_nhanvien] = True
+  for x in lstCheck:
+    if x == False:
+      return False
+  return True
+
+
+def generate_neighbours(lstOrder, M):
+
+  neighbours = []
+
+  for i in range(len(lstOrder)):
+
+    curr_nv = lstOrder[i].id_nhanvien
+
+    for nv in range(M):
+      neighbour = copy.deepcopy(lstOrder)
+      if nv != curr_nv:
+        neighbour[i].id_nhanvien = nv
+        if checkTotal(neighbour, M) == True:
+          neighbours.append(neighbour)
+
+  return neighbours
+
+
+def hill_climbing_search(current, M):
+
+  while True:
+
+    E = []
+    neighbours = []
+
+    current_state = ham_luong_gia(current)
+    print("current state = ", current_state)
+
+    neighbours = generate_neighbours(current, M)
+
+    for x in neighbours:
+      E.append(ham_luong_gia(x))  
+
+    min_value = min(E) if E else current_state
+
+    if current_state <= min_value:
+        return (current, current_state)
+
+    else:
+      best_neighbours = [i for i, x in enumerate(E) if x == min_value]
+
+      neighbour = best_neighbours[random.randrange(len(best_neighbours))]
+
+      current = neighbours[neighbour]
+
+
 def main():
   order_list = []
   for i in range (2,N+2):
@@ -102,9 +169,18 @@ def main():
   initialize_state(order_list, M)
   
   print_order_list(order_list)
-  print("\nLợi nhuận = ", loi_nhuan(order_list))
-  # print_result(order_list, M)
-  # print("\nLợi nhuận = ", loi_nhuan(order_list))
   print("Lượng giá = ", ham_luong_gia(order_list))
 
-main()
+  final_state = hill_climbing_search(order_list, M)
+  print("Final List Order is: ")
+  print_order_list(final_state[0])
+  print("Final Min is: ")
+  print(final_state[1])
+  write_result(final_state[0])
+
+
+
+
+
+if __name__ == "__main__":
+    main()
